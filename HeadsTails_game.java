b29d13e8e9a073c6362ab7ba0727394f975bedbd
s1310254 <s1310254@u-aizu.ac.jp>
2024-06-28 12:42:30 +0900

    HoT: initial revision

---
+++ HeadsTails_game.java
@@ -0,0 +1,21 @@
+class HeadsTails_game{
+  public static void main(String[] args){
+    int head = 0;
+    int tail = 0;
+    
+    for(int i = 1 ; i <= 3 ; i++){
+      int res = new java.util.Random().nextInt(2);
+      System.out.print("Round " + i + ": ");
+      if(res == 0){
+        System.out.println("Heads");
+        head++;
+      }
+      else if(res == 1){
+        System.out.println("Tails");
+        tail++;
+      }
+    }
+
+  System.out.println("Heads: " + head + ", Tails: " + tail);
+  }
+}
